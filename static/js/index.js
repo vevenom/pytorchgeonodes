080c186305548053bd072bb2308b9fa85e13cf0a
vevenom <92.sinisa@gmail.com>
2024-04-16 13:03:12 +0200

    Added template

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,12 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
+var INTERP_BASE = "./static/interpolation/stacked_new";
+var NUM_INTERP_FRAMES = 104;
 
 var interp_images = [];
 function preloadInterpolationImages() {
-  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
+  for (var i = 1; i <= NUM_INTERP_FRAMES; i++) {
+    var path = INTERP_BASE + '/interpolate (' + String(i) + ').jpg';
     interp_images[i] = new Image();
     interp_images[i].src = path;
   }
